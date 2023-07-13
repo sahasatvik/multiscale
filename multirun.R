430e92a7514147e2440b284d4a3c32ec3e5adb3b
Satvik Saha <sahasatvik@gmail.com>
2023-07-13 20:25:02 -0300

    Added durations, peaks

--- multirun.R
+++ multirun.R
@@ -9,7 +9,7 @@ library(zoo)
 
 df <- tibble(t = integer(), I = double(), run = integer(), p = double())
 
-for (p in c(2, 4, 6, 8)) {
+for (p in c(2, 3, 4, 5, 6, 8)) {
         for (i in 1:100) {
                 filename <- sprintf("output/p%d/countdata_%03d.dat", p, i)
                 d <- read.csv(filename)
@@ -23,19 +23,55 @@ for (p in c(2, 4, 6, 8)) {
 }
 
 df <- mutate(df, p = as.factor(p))
+df <- df %>%
+                filter(I > 0) %>%
+                group_by(p) %>%
+                complete(run, t) %>%
+                ungroup() %>%
+                replace_na(list(I = 0))
 
 durations <- df %>%
                 group_by(run, p) %>%
                 mutate(ma = rollapply(I, 30, mean, align = 'center', fill = 0.0)) %>%
                 mutate(dma = ma - lag(ma)) %>%
                 mutate(flip = dma * lag(dma) < 0) %>%
-                summarise(flips = sum(flip, na.rm = TRUE), duration = sum(I > 0))
+                summarise(
+                        flips = sum(flip, na.rm = TRUE),
+                        duration = sum(I > 0),
+                )
 
 averages <- df %>%
                 group_by(p, t) %>%
                 summarise(mean_I = mean(I))
 
-pdf(file = "multirun.pdf", width = 16, height = 8)
+peaks <- df %>%
+                group_by(p, run) %>%
+                mutate(KSmooth = ksmooth(t, I, "normal", bandwidth = 15, x.points = t)$y) %>%
+                mutate(
+                        diff1 = KSmooth - lag(KSmooth),
+                        diff2 = sign(diff1) - lag(sign(diff1)),
+                        peak = diff2 == -2
+                )
+
+peaks_n <- peaks %>%
+                summarise(peaks = sum(peak & (I > 1000)))
+
+peak_to_peak <- peaks %>%
+                filter(peak & (I > 1000)) %>%
+                filter(sum(peak) == 2) %>%
+                summarise(peak_to_peak = diff(t))
+
+heights <- df %>%
+                group_by(p, run) %>%
+                summarise(height = max(I))
+
+
+pdf(file = "multirun.pdf", width = 16, height = 10)
+
+sturges <- function(x) {
+        n = length(x)
+        ceiling(log(n, 2)) + 1
+}
 
 ggplot() +
         geom_line(
@@ -49,6 +85,7 @@ ggplot() +
                 aes(x = t, y = mean_I, group = p, color = p),
                 linewidth = 1.5
         ) +
+        ggtitle("Infection curves grouped by p, with averages") +
         scale_color_brewer(palette = "Set1") +
         theme_minimal()
 
@@ -63,15 +100,50 @@ ggplot() +
                 aes(x = t, y = mean_I, group = p, color = p),
                 linewidth = 1.5
         ) +
+        ggtitle("Infection curves separated by p, with averages") +
         scale_color_brewer(palette = "Set1") +
         facet_grid(p ~ ., scales = "free")
 
 ggplot() +
         geom_histogram(
                 data = durations,
-                aes(x = duration, fill = p)
+                aes(x = duration, fill = p),
+                bins = sturges(durations$duration)
         ) +
+        ggtitle("Epidemic durations grouped by p") +
         scale_fill_brewer(palette = "Set1") +
         facet_wrap(vars(p))
+
+peaks_n %>% count(peaks) %>% print(n = Inf)
+
+ggplot() +
+        geom_bar(
+                data = peaks_n %>%
+                        mutate(peaks = factor(peaks, levels = as.character(0:10))) %>%
+                        count(peaks),
+                aes(x = p, y = n, fill = peaks),
+                width = 1,
+                stat = "identity"
+        ) +
+        ggtitle("Number of peaks grouped by p") +
+        # coord_polar("y", start = 0) +
+        scale_fill_brewer(palette = "Set1")
+        # facet_wrap(vars(p))
+
+ggplot() +
+        geom_jitter(
+                data = peak_to_peak,
+                aes(x = p, y = peak_to_peak)
+        ) +
+        ggtitle("Peak to peak distances") +
+        scale_fill_brewer(palette = "Set1")
+
+ggplot() +
+        geom_jitter(
+                data = heights,
+                aes(x = p, y = height, color = p),
+        ) +
+        ggtitle("Epidemic heights grouped by p") +
+        scale_color_brewer(palette = "Set1")
 
 dev.off()
--- multirun.sh
+++ multirun.sh
@@ -3,7 +3,7 @@
 RUNS=100
 
 for i in $(seq -w 1 $RUNS); do
-        ./model 2> /dev/null
+        ./model
         mv output/countdata.dat "output/countdata_${i}.dat"
         mv output/averagedata.dat "output/averagedata_${i}.dat"
         echo "Run $i/$RUNS"
