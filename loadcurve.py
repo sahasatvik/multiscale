8cdad9f6c286b0db4ddcdcb288bdda5eed0dee6b
Satvik Saha <sahasatvik@gmail.com>
2023-07-17 15:28:53 -0300

    Fit mean infection curves to SIRS model

---
+++ loadcurve.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+from sirtiva import curve
+
+def load_curve(p, n):
+    filename = "output/p%d/countdata_%03d.dat" % (p, n)
+    df = pd.read_csv(filename)
+    df = df.rename(columns = str.strip)
+    return df[["Time", "Infected"]].to_numpy()
+
+
+def load_mean_curve(p, trials = 100, t_cut = 100):
+    data = list()
+    tmax = 0
+    for n in range(trials):
+        data.append(load_curve(p, n + 1))
+        tmax = max(tmax, data[n].shape[0])
+
+    ys = list()
+    for n in range(trials):
+        l = data[n].shape[0]
+        ys.append(np.pad(data[n][:, 1], (0, tmax - l), mode = 'constant'))
+
+    t = np.arange(0, tmax, 1.0)
+    y = np.mean(ys, axis = 0)
+
+    y = y[t < t_cut]
+    t = t[t < t_cut]
+
+    return y, t
+
+
+def show_fit(p, t_cut = 100, fig = 1):
+    y, t = load_mean_curve(p, t_cut = t_cut)
+
+    def f(t, beta, gamma, mu, I0):
+        t_ = np.arange(0, t.max() + 1, 1e-2)
+        y0 = (1e4, I0, 0.0)
+        sol = curve(t_, y0, beta, gamma, mu)
+        y = sol[t_ <= t.max()][::100]
+        y[y == np.inf] = 0
+        y[y == -np.inf] = 0
+        return np.nan_to_num(y)
+
+    p0 = (3.5e-1, 1 / 8, 0, 40)
+
+    popt, pcov, infodict, mesg, ier = curve_fit(f, t, y, p0 = p0, full_output = True, bounds = (0, [1, 1, 1, 1e3]))
+    print(p, popt)
+
+    plt.figure(fig)
+
+    plt.subplot(1, 2, 1)
+    plt.plot(t, y, label = "Simulated")
+    plt.plot(t, f(t, *popt), label = "Fitted")
+    # plt.plot(t, f(t, *p0), label = "Approximated")
+    plt.title("p = %f" % (p / 1000))
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(t, infodict["fvec"], label = "Residuals")
+    plt.legend()
+
+# p0 = (1e-5, 8.0e-9, 3.0e-1)
+
+# popt, pcov, infodict, mesg, ier = curve_fit(f, t, y, bounds = ([0, 0, 1e-3], [1e-3, 1e-7, 10]), full_output = True, nan_policy = 'omit', check_finite = False)
+# print(popt, pcov, infodict, mesg, ier)
+
+
+# print(popt)
+
+cutoffs = {
+    2: 100,
+    3: 100,
+    4: 100,
+    5: 100,
+    6: 100,
+    8: 100
+}
+
+fig = 1
+for p, t_cut in cutoffs.items():
+    show_fit(p, t_cut = t_cut, fig = fig)
+    fig += 1
+
+plt.show()
--- sirtiva.py
+++ sirtiva.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from ddeint import ddeint
+from scipy.integrate import odeint
 
 def beta_I(V, I, beta_I0 = 1e-4, C_V = 1e-6, C_I = 0):
     return (beta_I0 + C_V * V) / (1 + C_I * I)
@@ -21,15 +22,16 @@ def f(
     q = 2e0, p = 2e1, c = 1e-1,
     A0 = 1e0, delta_A = 2e-2, kappa_A = 1e-4,
     alpha = 5e-1, c_A = 5e-2,
-    epsilon = 1e0,
+    epsilon = 2e-1,
     gamma = 1e-1, mu = 1e-2,
-    xi = 1.0, delta_Z = 1.0, eta = 1e-6
+    xi = 1.0, delta_Z = 1.0, eta = 1e-6,
+    beta_I0 = 0.0, C_V = 1e-8, C_I = 0
 ):
     S, I, R, Z, T, U, V, A = y(t)
     _, _, _, _, _, _, V_, A_ = y(t - tau)
     return np.array([
-        -beta_I(V, I) * S * I - beta_Z(Z) * S * Z + mu * R, \
-        beta_I(V, I) * S * I + beta_Z(Z) * S * Z - gamma * I, \
+        -beta_I(V, I, beta_I0 = beta_I0, C_V = C_V, C_I = C_I) * S * I - beta_Z(Z) * S * Z + mu * R, \
+        beta_I(V, I, beta_I0 = beta_I0, C_V = C_V, C_I = C_I) * S * I + beta_Z(Z) * S * Z - gamma * I, \
         gamma * I - mu * R, \
         xi * V - delta_Z * Z, \
         (delta * (T0 - T) - kappa * T * V / (1 + alpha * A)) / epsilon, \
@@ -39,17 +41,28 @@ def f(
     ])
 
 
-t = np.arange(0, 100, 1e-2)
-y0 = lambda t: (1e4, 1.0, 0, 0, 1e4, 0, 1e-1, 1.0)
-sol = ddeint(f, y0, t)
+def curve(t, y0, beta, gamma, mu):
+    # g = lambda y, t: f(y, t, beta_I0 = beta_I0, C_V = C_V, epsilon = epsilon)
+    # return ddeint(g, y0, t)[:, 1]
+    def g(y, t):
+        S, I, R = y
+        N = S + I + R
+        return -beta * S * I / N + mu * R, beta * S * I / N - gamma * I, gamma * I - mu * R
+    return odeint(g, y0, t)[:, 1]
 
-plt.figure(1)
-plt.yscale("log")
-plt.plot(t, sol[:, 3:], label = ["Z", "T", "T*", "V", "A"])
-plt.ylim([1e-4, 1e5])
-plt.legend()
 
-plt.figure(2)
-plt.plot(t, sol[:, :3], label = ["S", "I", "R"])
-plt.legend()
-plt.show()
+if __name__ == '__main__':
+    t = np.arange(0, 100, 1e-2)
+    y0 = lambda t: (1e4, 1.0, 0, 0, 1e4, 0, 1e-1, 1.0)
+    sol = ddeint(f, y0, t)
+
+    plt.figure(1)
+    plt.yscale("log")
+    plt.plot(t, sol[:, 3:], label = ["Z", "T", "T*", "V", "A"])
+    plt.ylim([1e-4, 1e5])
+    plt.legend()
+
+    plt.figure(2)
+    plt.plot(t, sol[:, :3], label = ["S", "I", "R"])
+    plt.legend()
+    plt.show()
