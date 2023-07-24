ef40df40665efe96ba0ff345dda34e092aca2f45
Satvik Saha <sahasatvik@gmail.com>
2023-07-24 16:42:08 -0300

    Retain all output files

--- multirun.sh
+++ multirun.sh
@@ -7,5 +7,7 @@ for i in $(seq -w $RUNS_START $RUNS_END); do
         ./model
         mv output/countdata.dat "output/countdata_${i}.dat"
         mv output/averagedata.dat "output/averagedata_${i}.dat"
+        mv output/agentdata.dat "output/agentdata_${i}.dat"
+        mv output/environmentdata.dat "output/environmentdata_${i}.dat"
         echo "Run $i/$RUNS_END"
 done
