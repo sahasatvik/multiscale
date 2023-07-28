e433133963515cd7b97e6a8adfdf6069d3a5d1c1
Satvik Saha <sahasatvik@gmail.com>
2023-07-27 23:05:50 -0300

    Added script to compare infection curves and average viral loads

---
+++ infection_vs_viral.R
@@ -0,0 +1,37 @@
+#!/usr/bin/env Rscript
+
+library(tibble)
+library(dplyr)
+library(ggplot2)
+
+trials <- 1:200
+trial_p <- c(2, 3, 4, 5, 6, 8)
+
+for (p in trial_p) {
+
+        pdf(file = sprintf("figures/IvsV_%d.pdf", p), width = 8, height = 6)
+
+        for (i in trials) {
+
+                print(c(p, i))
+
+                countdata <- read.csv(sprintf("output/p%d/countdata_%03d.dat", p, i))
+                colnames(countdata) <- c("t", "S", "I", "R", "newI")
+                countdata <- tibble(countdata)
+
+                avgdata <- read.csv(sprintf("output/p%d/averagedata_%03d.dat", p, i))
+                colnames(avgdata) <- c("t", "T", "T*", "V", "A")
+                avgdata <- tibble(avgdata)
+
+                df <- inner_join(countdata, avgdata, by = 't')
+                df <- mutate(df, beta = newI / (S * I))
+
+                pl <- ggplot(df) +
+                        geom_line(aes(x = t, y = I, color = "Infections")) +
+                        geom_line(aes(x = t, y = V, color = "Viral load"))
+
+                print(pl)
+        }
+
+        dev.off()
+}
