dd2ac99a846fb6361145a6bad45e614248cf73df
Satvik Saha <sahasatvik@gmail.com>
2023-07-31 19:35:49 -0300

    Supplied p values to multirun.sh

--- multirun.sh
+++ multirun.sh
@@ -2,12 +2,16 @@
 
 RUNS_START=$1
 RUNS_END=$2
+P_INFECT=(2 3 4 5 6 8)
 
-for i in $(seq -w $RUNS_START $RUNS_END); do
-        ./model
-        mv output/countdata.dat "output/countdata_${i}.dat"
-        mv output/averagedata.dat "output/averagedata_${i}.dat"
-        mv output/agentdata.dat "output/agentdata_${i}.dat"
-        mv output/environmentdata.dat "output/environmentdata_${i}.dat"
-        echo "Run $i/$RUNS_END"
+for p in "${P_INFECT[@]}"; do
+        mkdir -p "output/p$p"
+        for i in $(seq -w $RUNS_START $RUNS_END); do
+                ./model -p ${p}e-3
+                mv output/countdata.dat "output/p$p/countdata_${i}.dat"
+                mv output/averagedata.dat "output/p$p/averagedata_${i}.dat"
+                mv output/agentdata.dat "output/p$p/agentdata_${i}.dat"
+                mv output/environmentdata.dat "output/p$p/environmentdata_${i}.dat"
+                "Run $i/$RUNS_END"
+        done
 done
